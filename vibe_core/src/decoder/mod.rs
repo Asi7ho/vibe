791c036b7483dbc91f04057bf3b95584e80afd37
Asi7ho <46624642+Asi7ho@users.noreply.github.com>
2021-01-06 17:04:59 +0100

    mp3 decoder

---
+++ vibe_core/src/decoder/mod.rs
@@ -0,0 +1,121 @@
+use std::path::Path;
+
+use crate::AudioInfo;
+use crate::Sample;
+
+#[cfg(feature = "mp3")]
+mod mp3;
+
+/// Audio decoder
+///
+/// For now support only mp3
+/// In the future will support FLAC, WAV, OGG and MP3
+pub struct Decoder {
+    decoder: FormatDecoder,
+}
+
+impl Decoder {
+    /// Used to open an audio file, the right decoder is chosen by looking at the extention of the file
+    #[inline]
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, ()> {
+        Ok(Self {
+            decoder: FormatDecoder::open(path)?,
+        })
+    }
+}
+
+impl Decoder {
+    /// Get the info of the file (Format, Sampling rate and Number of channels)
+    #[inline]
+    pub fn info(&self) -> AudioInfo {
+        self.decoder.info()
+    }
+
+    /// Return an iterator over the samples
+    #[inline]
+    pub fn into_samples(self) -> Result<SampleIterator, ()> {
+        self.decoder.into_samples()
+    }
+}
+/// Sample iterator to read the decoded samples (the channels are interleaved)
+pub struct SampleIterator(Box<dyn Iterator<Item = Result<Sample, ()>>>);
+
+impl Iterator for SampleIterator {
+    type Item = Result<Sample, ()>;
+
+    #[inline(always)]
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+}
+
+/// Choose the right decoder
+pub(crate) enum FormatDecoder {
+    #[cfg(feature = "wav")]
+    Wav(self::wav::WavDecoder),
+    #[cfg(feature = "vorbis")]
+    Vorbis(self::vorbis::VorbisDecoder),
+    #[cfg(feature = "mp3")]
+    Mp3(self::mp3::Mp3Decoder),
+    #[cfg(feature = "flac")]
+    Flac(self::flac::FlacDecoder),
+}
+
+impl FormatDecoder {
+    #[inline]
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, ()> {
+        // Check if feature exists
+        macro_rules! get_decoder {
+            ($in_ext:expr, $($ext:literal => requires $feature:literal for $init:expr),*) => {
+                match $in_ext {
+                    $(
+                        #[cfg(feature = $feature)]
+                        $ext => { return Ok($init) }
+                        #[cfg(not(feature = $feature))]
+                        $ext => { panic!("Feature not implemented yet") }
+                    )*
+                    other => panic!("Error: {:?}", other.to_owned())
+                }
+            }
+        }
+
+        // Look at the extention
+        if let Some(ext) = path.as_ref().extension().and_then(|ext| ext.to_str()) {
+            get_decoder!(ext,
+                "wav" => requires "wav" for FormatDecoder::Wav(self::wav::WavDecoder::open(path)?),
+                "ogg" => requires "vorbis" for FormatDecoder::Vorbis(self::vorbis::VorbisDecoder::open(path)?),
+                "mp3" => requires "mp3" for FormatDecoder::Mp3(self::mp3::Mp3Decoder::open(path)?),
+                "flac" => requires "flac" for FormatDecoder::Flac(self::flac::FlacDecoder::open(path)?)
+            )
+        }
+        panic!("Error")
+    }
+
+    #[inline]
+    pub fn into_samples(self) -> Result<SampleIterator, ()> {
+        match self {
+            #[cfg(feature = "wav")]
+            FormatDecoder::Wav(d) => Ok(SampleIterator(d.into_samples()?)),
+            #[cfg(feature = "vorbis")]
+            FormatDecoder::Vorbis(d) => Ok(SampleIterator(d.into_samples()?)),
+            #[cfg(feature = "mp3")]
+            FormatDecoder::Mp3(d) => Ok(SampleIterator(d.into_samples()?)),
+            #[cfg(feature = "flac")]
+            FormatDecoder::Flac(d) => Ok(SampleIterator(d.into_samples()?)),
+        }
+    }
+
+    #[inline]
+    pub fn info(&self) -> AudioInfo {
+        match self {
+            #[cfg(feature = "wav")]
+            FormatDecoder::Wav(d) => d.info(),
+            #[cfg(feature = "vorbis")]
+            FormatDecoder::Vorbis(d) => d.info(),
+            #[cfg(feature = "mp3")]
+            FormatDecoder::Mp3(d) => d.info(),
+            #[cfg(feature = "flac")]
+            FormatDecoder::Flac(d) => d.info(),
+        }
+    }
+}
---
+++ vibe_core/src/decoder/mp3.rs
@@ -0,0 +1,100 @@
+use minimp3::{Decoder, Error, Frame};
+use std::{fs::File, io::Read, path::Path};
+
+use crate::AudioFormat;
+use crate::AudioInfo;
+use crate::Sample;
+
+///Decoder for MP3 files
+pub struct Mp3Decoder {
+    decoder: Decoder<File>,
+    first_frame: Frame,
+    sample_rate: u32,
+    channels: usize,
+}
+
+impl Mp3Decoder {
+    /// Open MP3 file and create a decoder
+    #[inline]
+    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, ()> {
+        let f = File::open(path).map_err(|_| ())?;
+
+        let mut decoder = Decoder::new(f);
+
+        let first_frame = decoder.next_frame().map_err(|_| ())?;
+        let sample_rate = first_frame.sample_rate as u32;
+        let channels = first_frame.channels;
+
+        Ok(Mp3Decoder {
+            decoder: decoder,
+            first_frame: first_frame,
+            sample_rate: sample_rate,
+            channels: channels,
+        })
+    }
+
+    /// Get the info
+    #[inline]
+    pub fn info(&self) -> AudioInfo {
+        AudioInfo {
+            format: AudioFormat::Mp3,
+            sample_rate: self.sample_rate,
+            channels: self.channels,
+        }
+    }
+
+    /// Create iterator over the samples
+    #[inline]
+    pub fn into_samples(self) -> Result<Box<dyn Iterator<Item = Result<Sample, ()>>>, ()> {
+        Ok(Box::new(Mp3SampleIterator {
+            decoder: self.decoder,
+            channels: self.channels,
+            sample_rate: self.sample_rate,
+            current_frame: self.first_frame,
+            frame_cursor: 0,
+        }))
+    }
+}
+
+struct Mp3SampleIterator<R: Read> {
+    decoder: Decoder<R>,
+    channels: usize,
+    sample_rate: u32,
+    current_frame: Frame,
+    frame_cursor: usize,
+}
+
+impl<R: Read> Iterator for Mp3SampleIterator<R> {
+    type Item = Result<Sample, ()>;
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        // Read next frame in if current frame is exhausted
+        if self.frame_cursor >= self.current_frame.data.len() {
+            self.frame_cursor = 0;
+            self.current_frame = loop {
+                match self.decoder.next_frame() {
+                    Ok(frame) => {
+                        // Skip empty frames
+                        if frame.data.len() == 0 {
+                            continue;
+                        }
+
+                        // Make sure the sample rates and channels match
+                        assert_eq!(frame.sample_rate as u32, self.sample_rate);
+                        assert_eq!(frame.channels, self.channels);
+
+                        break frame;
+                    }
+                    Err(Error::SkippedData) => continue,
+                    Err(Error::Eof) => return None,
+                    Err(e) => panic!("Error: {:?}", e),
+                }
+            };
+        }
+
+        let sample_float = self.current_frame.data[self.frame_cursor] as f32 / i16::MAX as f32;
+        self.frame_cursor += 1;
+        Some(Ok(sample_float))
+    }
+}
