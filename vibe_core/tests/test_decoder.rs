ea1898b1fc0f15cbb88d75440142e19b03b6d1c4
Asi7ho <46624642+Asi7ho@users.noreply.github.com>
2021-01-06 20:27:53 +0100

    add test wav

--- vibe_core/tests/test_decoder.rs
+++ vibe_core/tests/test_decoder.rs
@@ -28,3 +28,31 @@ fn test_mp3_decoding() {
     println!("{} samples(s) read.", num_samples / info.channels());
     println!("Duration: {}ms", duration);
 }
+
+#[test]
+fn test_wav_decoding() {
+    // Test on wav
+    println!("Test WAV!");
+    let decoder = Decoder::open("tests/sounds/Test1.wav").unwrap();
+
+    let info = decoder.info();
+
+    println!("Format: {}", info.format());
+    assert_eq!(info.sample_rate(), 48000); // Sample rate is just
+    assert_eq!(info.channels(), 2); // Number of channels is just
+
+    let mut num_samples: usize = 0;
+
+    let sample_inter = decoder.into_samples().unwrap();
+    for sample in sample_inter {
+        match sample {
+            Ok(_) => num_samples += 1,
+            _ => continue,
+        }
+    }
+
+    let duration =
+        num_samples as u64 * 1_000 / (info.channels() as u64 * info.sample_rate() as u64);
+    println!("{} samples(s) read.", num_samples / info.channels());
+    println!("Duration: {}ms", duration);
+}
