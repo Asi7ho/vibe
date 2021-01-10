917a88cd0b6c7b37b27263d1a1605115df883f20
Asi7ho <46624642+Asi7ho@users.noreply.github.com>
2021-01-10 01:23:47 +0100

    use mod for tests

--- vibe_core/tests/test_decoder.rs
+++ vibe_core/tests/test_decoder.rs
@@ -1,114 +1,118 @@
-use vibe_core::decoder::Decoder;
+#[cfg(test)]
 
-#[test]
+mod tests_decoder {
+    use vibe_core::decoder::Decoder;
 
-fn test_mp3_decoding() {
-    // Test on mp3
-    println!("Test MP3!");
-    let decoder = Decoder::open("tests/sounds/Test1.mp3").unwrap();
+    #[test]
 
-    let info = decoder.info();
+    fn test_mp3_decoding() {
+        // Test on mp3
+        println!("Test MP3!");
+        let decoder = Decoder::open("tests/sounds/Test1.mp3").unwrap();
 
-    println!("Format: {}", info.format());
-    assert_eq!(info.sample_rate(), 44100); // Sample rate is just
-    assert_eq!(info.channels(), 2); // Number of channels is just
+        let info = decoder.info();
 
-    let mut num_samples: usize = 0;
+        println!("Format: {}", info.format());
+        assert_eq!(info.sample_rate(), 44100); // Sample rate is just
+        assert_eq!(info.channels(), 2); // Number of channels is just
 
-    let sample_inter = decoder.into_samples().unwrap();
-    for sample in sample_inter {
-        match sample {
-            Ok(_) => num_samples += 1,
-            _ => continue,
+        let mut num_samples: usize = 0;
+
+        let sample_inter = decoder.into_samples().unwrap();
+        for sample in sample_inter {
+            match sample {
+                Ok(_) => num_samples += 1,
+                _ => continue,
+            }
         }
+
+        let duration =
+            num_samples as u64 * 1_000 / (info.channels() as u64 * info.sample_rate() as u64);
+        println!("{} samples(s) read.", num_samples / info.channels());
+        println!("Duration: {}ms", duration);
     }
 
-    let duration =
-        num_samples as u64 * 1_000 / (info.channels() as u64 * info.sample_rate() as u64);
-    println!("{} samples(s) read.", num_samples / info.channels());
-    println!("Duration: {}ms", duration);
-}
+    #[test]
+    fn test_wav_decoding() {
+        // Test on wav
+        println!("Test WAV!");
+        let decoder = Decoder::open("tests/sounds/Test1.wav").unwrap();
 
-#[test]
-fn test_wav_decoding() {
-    // Test on wav
-    println!("Test WAV!");
-    let decoder = Decoder::open("tests/sounds/Test1.wav").unwrap();
+        let info = decoder.info();
 
-    let info = decoder.info();
+        println!("Format: {}", info.format());
+        assert_eq!(info.sample_rate(), 48000); // Sample rate is just
+        assert_eq!(info.channels(), 2); // Number of channels is just
 
-    println!("Format: {}", info.format());
-    assert_eq!(info.sample_rate(), 48000); // Sample rate is just
-    assert_eq!(info.channels(), 2); // Number of channels is just
+        let mut num_samples: usize = 0;
 
-    let mut num_samples: usize = 0;
-
-    let sample_inter = decoder.into_samples().unwrap();
-    for sample in sample_inter {
-        match sample {
-            Ok(_) => num_samples += 1,
-            _ => continue,
+        let sample_inter = decoder.into_samples().unwrap();
+        for sample in sample_inter {
+            match sample {
+                Ok(_) => num_samples += 1,
+                _ => continue,
+            }
         }
+
+        let duration =
+            num_samples as u64 * 1_000 / (info.channels() as u64 * info.sample_rate() as u64);
+        println!("{} samples(s) read.", num_samples / info.channels());
+        println!("Duration: {}ms", duration);
     }
 
-    let duration =
-        num_samples as u64 * 1_000 / (info.channels() as u64 * info.sample_rate() as u64);
-    println!("{} samples(s) read.", num_samples / info.channels());
-    println!("Duration: {}ms", duration);
-}
+    #[test]
+    fn test_flac_decoding() {
+        // Test on flac
+        println!("Test FLAC!");
+        let decoder = Decoder::open("tests/sounds/Test1.flac").unwrap();
 
-#[test]
-fn test_flac_decoding() {
-    // Test on flac
-    println!("Test FLAC!");
-    let decoder = Decoder::open("tests/sounds/Test1.flac").unwrap();
+        let info = decoder.info();
 
-    let info = decoder.info();
+        println!("Format: {}", info.format());
+        assert_eq!(info.sample_rate(), 48000); // Sample rate is just
+        assert_eq!(info.channels(), 2); // Number of channels is just
 
-    println!("Format: {}", info.format());
-    assert_eq!(info.sample_rate(), 48000); // Sample rate is just
-    assert_eq!(info.channels(), 2); // Number of channels is just
+        let mut num_samples: usize = 0;
 
-    let mut num_samples: usize = 0;
-
-    let sample_inter = decoder.into_samples().unwrap();
-    for sample in sample_inter {
-        match sample {
-            Ok(_) => num_samples += 1,
-            _ => continue,
+        let sample_inter = decoder.into_samples().unwrap();
+        for sample in sample_inter {
+            match sample {
+                Ok(_) => num_samples += 1,
+                _ => continue,
+            }
         }
+
+        let duration =
+            num_samples as u64 * 1_000 / (info.channels() as u64 * info.sample_rate() as u64);
+        println!("{} samples(s) read.", num_samples / info.channels());
+        println!("Duration: {}ms", duration);
     }
 
-    let duration =
-        num_samples as u64 * 1_000 / (info.channels() as u64 * info.sample_rate() as u64);
-    println!("{} samples(s) read.", num_samples / info.channels());
-    println!("Duration: {}ms", duration);
-}
+    #[test]
+    fn test_ogg_decoding() {
+        // Test on ogg
+        println!("Test OGG!");
+        let decoder = Decoder::open("tests/sounds/Test1.ogg").unwrap();
 
-#[test]
-fn test_ogg_decoding() {
-    // Test on ogg
-    println!("Test OGG!");
-    let decoder = Decoder::open("tests/sounds/Test1.ogg").unwrap();
+        let info = decoder.info();
 
-    let info = decoder.info();
+        println!("Format: {}", info.format());
+        assert_eq!(info.sample_rate(), 44100); // Sample rate is just
+        assert_eq!(info.channels(), 2); // Number of channels is just
 
-    println!("Format: {}", info.format());
-    assert_eq!(info.sample_rate(), 44100); // Sample rate is just
-    assert_eq!(info.channels(), 2); // Number of channels is just
+        let mut num_samples: usize = 0;
 
-    let mut num_samples: usize = 0;
-
-    let sample_inter = decoder.into_samples().unwrap();
-    for sample in sample_inter {
-        match sample {
-            Ok(_) => num_samples += 1,
-            _ => continue,
+        let sample_inter = decoder.into_samples().unwrap();
+        for sample in sample_inter {
+            match sample {
+                Ok(_) => num_samples += 1,
+                _ => continue,
+            }
         }
-    }
 
-    let duration =
-        num_samples as u64 * 1_000 / (info.channels() as u64 * info.sample_rate() as u64);
-    println!("{} samples(s) read.", num_samples / info.channels());
-    println!("Duration: {}ms", duration);
+        let duration =
+            num_samples as u64 * 1_000 / (info.channels() as u64 * info.sample_rate() as u64);
+        println!("{} samples(s) read.", num_samples / info.channels());
+        println!("Duration: {}ms", duration);
+    }
 }
