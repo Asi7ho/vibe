9a8ac5e70df5a09b6b0c52be9767fd842cae33e1
Asi7ho <46624642+Asi7ho@users.noreply.github.com>
2021-01-14 17:18:08 +0100

    add hello world ui

--- vibe_gui/src/main.rs
+++ vibe_gui/src/main.rs
@@ -1,3 +1,19 @@
+use druid::{AppLauncher, WindowDesc};
+
+mod data;
+use data::AppState;
+
+mod view;
+use view::build_ui;
+
 pub fn main() {
-    println!("Hello, World!")
+    let main_window = WindowDesc::new(build_ui)
+        .title("Vibe Player")
+        .window_size((400.0, 120.0));
+
+    let initial_state = AppState {};
+
+    AppLauncher::with_window(main_window)
+        .launch(initial_state)
+        .expect("Failed to launch application");
 }
