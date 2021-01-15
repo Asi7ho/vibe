69af770a4f1de7d8dd37f83619e18f9c066c65e3
Asi7ho <46624642+Asi7ho@users.noreply.github.com>
2021-01-15 17:02:48 +0100

    get duration audio from player

--- vibe_engine/src/player/player.rs
+++ vibe_engine/src/player/player.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{fs::File, time::Duration};
 
 use vibe_core::decoder::Decoder;
 
@@ -7,24 +7,31 @@ use crate::stream::AudioStream;
 #[derive(Clone)]
 pub struct Player {
     stream: Option<AudioStream>,
+    stream_duration: Option<Duration>,
 }
 
 impl Player {
     #[inline]
     pub fn new() -> Result<Self, ()> {
-        Ok(Self { stream: None })
+        Ok(Self {
+            stream: None,
+            stream_duration: None,
+        })
     }
 
-    pub fn play_audio(&mut self, path: &str) {
-        if self.stream.is_some() {
-            // self.stream.as_ref().unwrap().stop();
-            self.stream = None;
-        }
-
+    pub fn create_stream(&mut self, path: &str) {
         let file = File::open(path).expect("File not found");
         let decoder = Decoder::new(file).expect("Decoding error");
 
+        let info = decoder.info();
+        let duration = info.duration();
+
         self.stream = Some(AudioStream::new::<f32, File>(decoder).unwrap());
+        self.stream_duration = duration;
+    }
+
+    pub fn stream_duration(&self) -> Option<Duration> {
+        self.stream_duration
     }
 
     pub fn play_stream(&self) {
